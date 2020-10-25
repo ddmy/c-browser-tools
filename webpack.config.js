abf6f1379e597a2c7bfc7675968a29ca30468c47
YiJio <841312998@qq.com>
2020-10-25 18:42:39 +0800

    修改webpaack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const isDev = process.env.NODE_ENV === 'development'
 
+const plugins = []
+
+if (isDev) {
+  plugins.push(
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    })
+  )
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+  )
+}
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'index.html')
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins,
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     hot: true,
