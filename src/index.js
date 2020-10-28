38731ce20914770012c24f2ecefd12c53d4339ce
YiJio <841312998@qq.com>
2020-10-28 22:02:18 +0800

    修复模块导出问题

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { BT, Cookie, Url, Image, localStorage } from './utils/index.js'
+import Tools from './utils/index.js'
 
 if (module.hot) {
   module.hot.accept();
 }
 
-console.log('index.js初始化')
+export default Tools
